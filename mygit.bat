f8edd580938b11cf996880389327d5d26f04967d
Nikhilhututu <nik87jain@gmail.com>
2019-08-01 13:40:13 +0530

    f5

---
+++ mygit.bat
@@ -0,0 +1,14 @@
+
+@ECHO OFF 
+ECHO Start update on git with Heroku....
+ECHO ===========Start==File Add===============
+git add .
+ECHO ===========Commit==File===============
+git commit -m "f5" 
+ECHO ===========push==File===============
+git push
+ECHO ===========pull==File===============
+git pull
+ECHO ===========push heroku==File===============
+git push heroku
+PAUSE
