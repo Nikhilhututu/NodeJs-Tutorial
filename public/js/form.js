541956c042d7683f7b044c4071c0c1109dc8579c
Nikhilhututu <nik87jain@gmail.com>
2019-08-01 13:26:36 +0530

    1-8-2019_2

--- public/js/form.js
+++ public/js/form.js
@@ -16,7 +16,7 @@ const forecast = require('./forcast')
 const express  = require('express') 
 const path     = require('path') 
 const app      = express()
-const port = 3001
+const port = process.env.PORT || 3000
 app.listen(port, () =>
 { 
    console.log('Server Port is=== '+port) 
