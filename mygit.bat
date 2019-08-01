7113ac3cda84fe44227714c3817bc07c55c18572
Nikhilhututu <nik87jain@gmail.com>
2019-08-01 13:59:33 +0530

    08/01/2019:13:59:33.20

--- mygit.bat
+++ mygit.bat
@@ -4,7 +4,9 @@ ECHO Start update on git with Heroku....
 ECHO ===========Start==File Add===============
 git add .
 ECHO ===========Commit==File===============
-git commit -m "f5" 
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+set mytime=%time%
+git commit -m %mydate%:%mytime% 
 ECHO ===========push==File===============
 git push
 ECHO ===========pull==File===============
